ecda1c7de24fe95a00909da2e7892ce7dbace1b5
Vadim Dimnich <v.dimnich@gmail.com>
2022-05-10 19:21:31 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryRun(boolean condition) {
+        System.out.println(
+                "Колобок " +  (condition ? "съеден" : "сбежал")
+        );
+    }
+}
