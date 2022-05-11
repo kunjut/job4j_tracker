0e1b047e88f44edad2a85b47658dccf2375f1e04
Vadim Dimnich <v.dimnich@gmail.com>
2022-05-11 13:10:29 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Есть ошибка: " + active);
+        System.out.println("Статус ошибки: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error empty = new Error();
+        empty.printInfo();
+        System.out.println();
+        Error notFound = new Error(true, 404, "Not found");
+        notFound.printInfo();
+        System.out.println();
+        Error notAcceptable = new Error(true, 406, "Not acceptable");
+        notAcceptable.printInfo();
+    }
+}
