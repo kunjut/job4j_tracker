f88240d89345959a9a6913ec19381135dbed522c
Vadim Dimnich <v.dimnich@gmail.com>
2022-06-02 08:40:53 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,15 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public int calc(int n) {
+        int rsl = 1;
+        for (int index = 2; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        new Fact().calc(-1);
+    }
+}
